24fa2b3b7991526af3aebadcf76b8893f5c817fd
Patricia Nykaza <pnykaza@calstatela.edu>
2018-09-13 19:58:43 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,8 +13,9 @@ class App < Sinatra::Base
 
   post '/checkout' do
     @item = params[:item]
-    session["item"] = @item
     @session = session
+    session["item"] = @item
+    # @session = session
     erb :checkout
   end
 end
