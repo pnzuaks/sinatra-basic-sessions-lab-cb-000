111b253bab9ff9431d8032efbff9a3b0836efce9
Patricia Nykaza <pnykaza@calstatela.edu>
2018-09-13 20:36:53 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,15 +8,16 @@ class App < Sinatra::Base
   end
 
   get '/' do
-    @session = session
-
     erb :index
   end
 
   post '/checkout' do
     @item = params[:item]
-    # session["item"] = @item
-    # @session = session
+    session[:item] = @item
+    @session = session
+
+    # store_name("names.txt", @name)
+    # session[:message] = "Successfully stored the name #{@name}."
     erb :checkout
   end
 end
