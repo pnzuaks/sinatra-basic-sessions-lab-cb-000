8d4c9a7d9026fb3b46ddf32ee1d26dfcf08b1853
Patricia Nykaza <pnykaza@calstatela.edu>
2018-09-13 19:47:17 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,11 +7,13 @@ end
 
 class App < Sinatra::Base
   get '/' do
+    @session = session
     erb :index
   end
 
   post '/checkout' do
     @item = params[:item]
+    session[item] = 
     @session = session
   end
 end
